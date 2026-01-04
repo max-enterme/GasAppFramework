f1352d477db1db115c6a97744d9a912fe6c9452a
MAX <max.enterme@gmail.com>
2026-01-04 22:58:33 +0900

    fix

--- modules/testing/index.ts
+++ modules/testing/index.ts
@@ -8,6 +8,9 @@ export * as Assert from './Assert';
 export * as GasReporter from './GasReporter';
 export * as LogCapture from './LogCapture';
 
+// Named exports for shared-tests compatibility
+export { it, all, byCategory, categories, getCaseWithCategory, clear } from './Test';
+
 // Export TestHelpers as a namespace
 import * as TH from './TestHelpers';
 export { TH as TestHelpers };
